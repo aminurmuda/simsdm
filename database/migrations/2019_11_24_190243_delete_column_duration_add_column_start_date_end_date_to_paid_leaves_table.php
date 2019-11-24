986ca70f0f1218b582432281a17c9a08c1630784
Aminur Muda <aminurmuda@gmail.com>
2019-11-24 20:46:38 +0700

    Update 2019_11_24_190243_delete_column_duration_add_column_start_date_end_date_to_paid_leaves_table.php

--- database/migrations/2019_11_24_190243_delete_column_duration_add_column_start_date_end_date_to_paid_leaves_table.php
+++ database/migrations/2019_11_24_190243_delete_column_duration_add_column_start_date_end_date_to_paid_leaves_table.php
@@ -18,6 +18,7 @@ class DeleteColumnDurationAddColumnStartDateEndDateToPaidLeavesTable extends Mig
             $table->date('start_date');
             $table->date('end_date');
             $table->string('reason');
+            $table->string('reject_reason')->nullable();
             $table->timestamps();
         });
     }
@@ -31,7 +32,7 @@ class DeleteColumnDurationAddColumnStartDateEndDateToPaidLeavesTable extends Mig
     {
         Schema::table('paid_leaves', function (Blueprint $table) {
             $table->integer('duration');
-            $table->dropColumn(['start_date', 'end_date', 'reason', 'created_at', 'updated_at']);
+            $table->dropColumn(['start_date', 'end_date', 'reason', 'reject_reason', 'created_at', 'updated_at']);
 
         });
     }
